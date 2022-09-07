ba31464bd2fbd312ac2cbec76185b7aaa03e1a4b
Lucas <lucascarmo6@gmail.com>
2022-09-07 18:54:57 -0300

    refactor: move input  to a separate file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,3 +1,4 @@
+import Input from '@/presentation/components/input/input'
 import Footer from '@/presentation/components/footer/footer'
 import LoginHeader from '@/presentation/components/loginHeader/loginHeader'
 import Spinner from '@/presentation/components/spinner/spinner'
@@ -5,35 +6,38 @@ import React from 'react'
 import Styles from './login.styles.scss'
 
 const Login: React.FC = () => {
-  return <div className={Styles.login}>
+  return (
+    <div className={Styles.login}>
+      {/* header section */}
+      <LoginHeader />
 
-    {/* header section */}
-    <LoginHeader />
+      {/* form section */}
+      <form className={Styles.form}>
+        <h2>Login</h2>
+        <Input type="email" name="email" placeholder="Digite seu email" />
+        <Input
+          type="password"
+          name="password"
+          id=""
+          placeholder="Digite sua senha"
+        />
 
-    {/* form section */}
-    <form className={Styles.form}>
-      <h2>Login</h2>
-      <div className={Styles.inputWrap}>
-        <input type="email" name="email" id="" placeholder='Digite seu email' />
-        <span className={Styles.status}>🔴</span>
-      </div>
-      <div className={Styles.inputWrap}>
-        <input type="password" name="password" id="" placeholder='Digite sua senha' />
-        <span className={Styles.status}>🔴</span>
-      </div>
-      <button className={Styles.submit} type="submit">Entrar</button>
-      <div className={Styles.linkContainer}>
-        <span className={Styles.link}>Criar conta</span>
-      </div>
-      <div className={Styles.errorWrap}>
-        <Spinner className={Styles.spinner} />
-        <span className={Styles.error}>erro</span>
-      </div>
-    </form>
+        <button className={Styles.submit} type="submit">
+          Entrar
+        </button>
+        <div className={Styles.linkContainer}>
+          <span className={Styles.link}>Criar conta</span>
+        </div>
+        <div className={Styles.errorWrap}>
+          <Spinner className={Styles.spinner} />
+          <span className={Styles.error}>erro</span>
+        </div>
+      </form>
 
-    {/* footer section */}
-    <Footer/>
-  </div>
+      {/* footer section */}
+      <Footer />
+    </div>
+  )
 }
 
 export default Login
